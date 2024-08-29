da3721d813b2541a62dbe1c86bc271369fe35637
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-08-29 17:08:59 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,93 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useMemo, useState } from 'react'
 
+const App = () => {
+
+    const[first, setFirst] = useState([])
+    const[count, setCount] = useState(0)
+    const[text, setText] = useState("")
+    const[todos, setTodos] = useState([])
+
+
+    const memoizedTodoCount = useMemo(() => first.length, [first]);
+  return (
+    <div>
+        
+        <h1>My Todos</h1>
+
+        <ul>
+            {
+                first.map((item) => {
+                    return <li>{item}</li>
+                })
+            }
+        </ul>
+
+
+        <button onClick={() => {
+            setFirst([...first, "New Todo"])
+        }}>Add Todo</button>
+
+
+
+        <hr></hr>
+
+
+
+        {count}
+        <button onClick={() => {
+            setCount(count + 1)
+        }}>+</button>
+
+
+        <hr></hr>
+
+
+        <h1>React.memo</h1>
+        <input type='text' value={text} onChange={(e) => {
+            setText(e.target.value)
+        }}/>
+        <button onClick={() => {
+            setTodos([...todos, text])
+            setText("")
+        }}>Add Skill</button>
+
+        <ul>
+            {todos.map((item) => {
+                return <li>{item}</li>
+            })}
+        </ul>
+
+
+        <hr></hr>
+
+
+
+        <UseMemoComponent count={memoizedTodoCount} />
+        <ReactMemoComponent text={text} />
+    </div>
+  )
+}
+
+export default App
+
+
+
+const UseMemoComponent = ({ count }) => {
+    return (
+        <div>
+            <h2>UseMemo Component</h2>
+            <p>Total todos: {count}</p>
+        </div>
+    );
+};
+
+
+const ReactMemoComponent = React.memo(({ text }) => {
+    console.log('React.memo component rendered!');
+    return (
+        <div>
+            <h2>React.memo Component</h2>
+            <p>Current input: {text}</p>
+        </div>
+    );
+});
--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
+const rootElement = document.getElementById('root');
+
+// Render the App component on the root DOM node
+ReactDOM.render(<App />, rootElement);
 
